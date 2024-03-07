0117d47aca2087502ad4cc969919cc555f5a6aa9
Magda Krejcirova <magda.krejcirova@sap.com>
2024-03-07 17:10:30 +0100

    nekupto

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ const product3 = {
   name: 'Anatoplavky',
   description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
+
+const product1HTML = `
+    <img class="card-img-top" src="images/destniklobouk.jpg" alt="Card image cap">
+    <div class="card-body">
+          <h2 class="card-title">Deštníklobouk</h2>
+          <p class="card-text">Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou
+            přeháňkou.</p>
+
+    </div>
+    <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`
+const product1Elm = document.querySelector('#product1');
+product1Elm.innerHTML = product1HTML;
